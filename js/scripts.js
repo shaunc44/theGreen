54b93dcc4fe1f0eabb6619fb4db5d8a9cfb1a38e
Shaun <seyyes2000@hotmail.com>
2016-05-09 21:04:21 -0400

    feat: Add Read More click event that open an alert box when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
 	// add 'fade-in' class to h1 tag after page loads
 	$('h1').addClass('fade-in');
 
+	// add click event to heading the prompts alert "Welcome, Smart Investor!" to user
+	$('h1').on('click', function() {
+		alert("Welcome, Smart Investor!");
+	});
+
+	// add click event to 'read more' that shows additional info
+	$('.read-more').on('click', function() {
+		$(this).hide("fast", function() {
+			alert("This webpage will help you visualize what your retirment savings will be at different ages and investment scenarios");
+		});
+	});
+
 });
